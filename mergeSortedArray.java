b33b77a37dba6a476d385896d575f49b79c5d6d6
Abhishek Prasad <devcula@outlook.com>
2023-12-22 17:12:41 +0530

    project setup

---
+++ mergeSortedArray.java
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/merge-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+
+class MergeSortedArray {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
