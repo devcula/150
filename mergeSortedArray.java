4b29da6b468329d51b5827f5925e9ba4e7ccb410
Abhishek Prasad <devcula@outlook.com>
2024-01-10 22:53:15 +0530

    changes

--- mergeSortedArray.java
+++ mergeSortedArray.java
@@ -2,6 +2,6 @@
 
 class MergeSortedArray {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("I will start from next week. I promise!");
     }
 }
